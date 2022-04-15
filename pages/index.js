9089d240d7fd735a90fd6e5f524d415d001f75b6
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-04-15 15:06:49 +0700

    add animated section component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a student in Brawijaya University!
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -13,7 +26,31 @@ const Page = () => {
           </Heading>
           <p>IT Enthusiast / Student / Developer</p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/dimas.jpg"
+            alt="Profile Image"
+          />
+        </Box>
       </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
