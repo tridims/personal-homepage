6aef41fb216590e24c0d37cbfc8889a6a37c79a6
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-04-15 20:58:28 +0700

    implement pages transition

--- pages/index.js
+++ pages/index.js
@@ -12,89 +12,92 @@ import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a student in Brawijaya University!
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Dimas Tri Mustakim
-          </Heading>
-          <p>IT Enthusiast / Student / Developer</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/dimas.jpg"
-            alt="Profile Image"
-          />
+          Hello, I&apos;m a student in Brawijaya University!
         </Box>
-      </Box>
-
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          I am a student in Brawijaya University in my second year. Currently I
-          have interest in Software Engineering, Full-Stack Development, Machine
-          Learning, and Security. Not yet to decide which one i would really
-          want, but i belive that learning all of those will be helpful in
-          building good stuff in the future. Pls chekout my website in{' '}
-          <NextLink href="https://www.dimastri.online">
-            <Link>here.</Link>
-          </NextLink>
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My Portofolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Dimas Tri Mustakim
+            </Heading>
+            <p>IT Enthusiast / Student / Developer</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/dimas.jpg"
+              alt="Profile Image"
+            />
+          </Box>
         </Box>
-      </Section>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>2001</BioYear>
-          Born in Batu, Jawa Timur.
-        </BioSection>
-        <BioSection>
-          <BioYear>2020</BioYear>
-          Started education in informatics at Brawijaya University.
-        </BioSection>
-      </Section>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            I am a student in Brawijaya University in my second year. Currently
+            I have interest in Software Engineering, Full-Stack Development,
+            Machine Learning, and Security. Not yet to decide which one i would
+            really want, but i belive that learning all of those will be helpful
+            in building good stuff in the future. Pls chekout my website in{' '}
+            <NextLink href="https://www.dimastri.online">
+              <Link>here.</Link>
+            </NextLink>
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My Portofolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          Hobbys ❤️
-        </Heading>
-        <Paragraph>Exploring tech related stuff, reading manga.</Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>2001</BioYear>
+            Born in Batu, Jawa Timur.
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Started education in informatics at Brawijaya University.
+          </BioSection>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Hobbys ❤️
+          </Heading>
+          <Paragraph>Exploring tech related stuff, reading manga.</Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,39 +1,42 @@
 import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 
 import thumbJFrameApp from '../public/images/works/ThumbJFrameApp.png'
 import thumbSimpleApp from '../public/images/works/simpleAppCollection.png'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem
-            id="JFrame App"
-            title="JFrame App for managing cow sales"
-            thumbnail={thumbJFrameApp}
-          >
-            A simple Java GUI Application to keep track of cow sales.
-          </WorkGridItem>
-        </Section>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="JFrame App"
+              title="JFrame App for managing cow sales"
+              thumbnail={thumbJFrameApp}
+            >
+              A simple Java GUI Application to keep track of cow sales.
+            </WorkGridItem>
+          </Section>
 
-        <Section>
-          <WorkGridItem
-            id="Simple App"
-            title="A Collection of Simple App"
-            thumbnail={thumbSimpleApp}
-          >
-            A collection of simple project that i wrote.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+          <Section>
+            <WorkGridItem
+              id="Simple App"
+              title="A Collection of Simple App"
+              thumbnail={thumbSimpleApp}
+            >
+              A collection of simple project that i wrote.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
