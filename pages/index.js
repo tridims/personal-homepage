1469892cb4747a0e08adf8e8d7c5841cb4f12dba
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-04-15 16:20:10 +0700

    add bio section
    Adding bio for timeline in my education and hobbys

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import NextLink from 'next/link'
 import {
+  Button,
   Container,
   Box,
   Heading,
+  Link,
   Image,
   useColorModeValue,
 } from '@chakra-ui/react'
 import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
@@ -49,7 +55,44 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          I am a student in Brawijaya University in my second year. Currently I
+          have interest in Software Engineering, Full-Stack Development, Machine
+          Learning, and Security. Not yet to decide which one i would really
+          want, but i belive that learning all of those will be helpful in
+          building good stuff in the future. Pls chekout my website in{' '}
+          <NextLink href="https://www.dimastri.online">
+            <Link>here.</Link>
+          </NextLink>
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Portofolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2001</BioYear>
+          Born in Batu, Jawa Timur.
+        </BioSection>
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Started education in informatics at Brawijaya University.
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          Hobbys ❤️
+        </Heading>
+        <Paragraph>Exploring tech related stuff, reading manga.</Paragraph>
       </Section>
     </Container>
   )
