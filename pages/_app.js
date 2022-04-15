88014e2c3aac79c87d1c27d705d0abaf30e673f4
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-04-15 14:17:02 +0700

    add change theme function and change the font that is used

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/provider'
 import Layout from '../components/layouts/main'
+import theme from '../lib/theme'
+import Fonts from '../components/fonts'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
