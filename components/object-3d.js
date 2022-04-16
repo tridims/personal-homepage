6d1a1735894828b2e012c87cb23431bdeeab1ed4
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-04-16 11:12:24 +0700

    add 404 custom page

--- components/object-3d.js
+++ components/object-3d.js
@@ -125,7 +125,7 @@ const Object3d = () => {
       ref={refContainer}
       className="3d-object"
       m="auto"
-      at={['-20px', '-60px', '-120px']}
+      mt={['-20px', '-60px', '-120px']}
       mb={['-40px', '-140px', '-200px']}
       w={[280, 480, 640]}
       h={[280, 480, 640]}
